e17e783cabc62381d06c15b786339b1b07c2db73
lkon-hub <77855147+lkon-hub@users.noreply.github.com>
2021-01-22 19:51:59 +0100

    Bug fixed on adding new tags
    Adding new tags was not possible when the id-field and text-field were changed.

--- src/VoerroTagsInput.vue
+++ src/VoerroTagsInput.vue
@@ -395,10 +395,10 @@ export default {
 
                     // Determine if the inputted tag exists in the typeagedTags
                     // array
-                    let newTag = {
-                        key: '',
-                        value: text,
-                    };
+                    let newTag = { };
+
+                    newTag[this.idField] = '';
+                    newTag[this.textField] = text;
 
                     const searchQuery = this.escapeRegExp(
                         this.caseSensitiveTags
